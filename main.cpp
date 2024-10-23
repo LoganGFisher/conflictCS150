cbe71c6f6dc3c99fb26944ea4f8627c22bdd0f12
LoganGFisher <lgfisher@nic.edu>
2024-10-23 17:26:19 +0000

    Nope

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ int main()
 {
     cout<<"I am a Big Nerd\n";
     cout<<"I am a Little Nerd\n";
+    cout<<nope()<<endl;
     return 0;
+}
+int nope()
+{
+    int nerd = 1 + 2;
+    return nerd;
 }
